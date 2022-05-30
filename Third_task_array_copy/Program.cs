96687ffa0a55f31dd39c9f986e620e61ef63fb18
Dima <dima.brashovyan@mail.ru>
2022-05-30 08:36:22 +0300

    Программа копирования массива

---
+++ Third_task_array_copy/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа копирования массива.
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int arrSize = int.Parse(Console.ReadLine()?? "");
+int[] array = new int [arrSize];
+for(int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1,10);
+for(int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i] + " "}");
+Console.WriteLine();
+
+int[] newArray = new int [array.Length];
+for(int j = 0; j < array.Length; j++)
+{
+    newArray[j] = array[j];
+    Console.Write($"{newArray[j]} ");
+}    
+Console.WriteLine();
