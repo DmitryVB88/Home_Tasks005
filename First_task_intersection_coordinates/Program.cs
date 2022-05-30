67c4395b5e47802a5047a258636dacfb8cc6fc83
Dima <dima.brashovyan@mail.ru>
2022-05-30 08:35:15 +0300

    Масштабирование фигуры

---
+++ First_task_intersection_coordinates/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением 
+// y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы.
+Console.Clear();
+Console.WriteLine("Для ввода коорднат принимаются только целые числа!");
+Console.Write("Введите координату k1 = ");
+int k1 = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите координату k2 = ");
+int k2 = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите координату b1 = ");
+int b1 = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите координату b2 = ");
+int b2 = int.Parse(Console.ReadLine()?? "");
+int x = (b2-b1)/(k1-k2);
+Console.WriteLine($"Координаты x = {x}");
+int y= k1* (b2-b1)/(k1-k2)+b1;
+Console.WriteLine($"Координаты y = {y}");
+
---
+++ Second_task_figure_scaling/Program.cs
@@ -0,0 +1,40 @@
+﻿// 2.Написать программу масштабирования фигуры
+
+Console.Clear();
+  Console.Write("Введите коэффициент K, для масштабирования фигуры: ");
+double k = Console.ReadLine();
+// int k = int.Parse(Console.ReadLine()?? "");
+double[,] matrix = new double [4, 2];
+
+   for(int row = 0; row < matrix.GetLength(0); row++)
+   {
+       for(int column = 0; column < matrix.GetLength(1); column++)
+       {
+         matrix[row, column] = new Random().NextDouble(1,5);
+       } 
+
+}    
+
+for(int row = 0; row < matrix.GetLength(0); row++)
+   {
+    for(int column = 0; column < matrix.GetLength(1); column++)
+       {
+        Console.Write($"{matrix[row,column] + " "}");
+       } 
+ Console.WriteLine();
+  }            
+
+for(int row = 0; row < matrix.GetLength(0); row++)
+   {
+    for(int column = 0; column < matrix.GetLength(1); column++)
+       {
+        Console.Write($"{matrix[row,column] * k + " "}");
+       } 
+Console.WriteLine();
+
+  }            
+
+//   Console.Write("Введите коэффициент K, для масштабирования фигуры: ");
+//   int k = int.Parse(Console.ReadLine()?? "");
+
+// matrix[row][column] = matrix[row][column] * 2;
